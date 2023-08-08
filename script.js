3de0d6788d2f2f64c9758ada29c94f11fa8180e1
Bukvic Armin <bukvicarmin@gmail.com>
2023-08-08 23:43:42 +0200

    Files

---
+++ script.js
@@ -0,0 +1,265 @@
+let movies = {
+
+    movie1: {
+        filesrc: "movie1",
+        title: "The Super Mario Bros. Movie",
+        length: "1h 32m",
+        rating: "95% audience score",
+        desc: "Italian-American brothers Mario and Luigi have recently started a plumbing business in Brooklyn, to the derision of their ex-employer Spike and the disapproval of their father. After seeing a significant water main leak on the news, Mario and Luigi go underground to fix it but are sucked into a Warp Pipe and separated.<br>Mario lands in the Mushroom Kingdom, ruled by Princess Peach, while Luigi lands in the Dark Lands, ruled by the evil Koopa king Bowser. Bowser seeks to marry Peach and will destroy the Mushroom Kingdom using a Super Star if she refuses. He imprisons Luigi to threaten Mario, whom he sees as competition for Peach's love. Mario meets Toad, who takes him to Peach. Peach plans to ally with the primate Kongs to help repel Bowser and allows Mario and Toad to travel with her. She also tells Mario that she ended up in the Mushroom Kingdom as a baby, where the Toads took her in and eventually made her their leader. In the Jungle Kingdom, King Cranky Kong agrees to help if Mario defeats his son, Donkey Kong, in a fight. Despite Donkey Kong's immense strength, Mario defeats him using a Cat Suit.",
+        color: "#be2323",
+        textcolor: "white"
+    },
+
+    movie2: {
+        filesrc: "movie2",
+        title: "Pokémon - Detective Pikachu",
+        length: "1h 44m",
+        rating: "79% audience score",
+        desc: "Tim Goodman gave up his dream of being a Pokémon trainer following the death of his mother and his estrangement from his father Harry, a police detective who moved to Ryme City, where humans and Pokémon live together in harmony and Pokémon battles are illegal. Tim is informed by Harry's friend, Lieutenant Hideo Yoshida, that Harry was killed in a car accident. Tim goes to Harry's apartment to sort things out and meets Harry's police partner, a deerstalker-clad amnesiac Pikachu, whom only Tim can understand. Tim accidentally releases a mysterious purple gas known as 'R' from a vial he finds in Harry's office; they are then attacked by a party of Aipom[d] who became rabid under the gas's influence.<br>Pikachu believes that Harry survived the crash, as the police never found his body. They meet an informant of Harry's, a Mr. Mime, who directs them to an illegal underground Pokémon battle arena. The arena is raided by police and Tim is brought to Yoshida, who reveals footage of Harry's crash, explaining that Harry having survived would have been impossible.",
+        color: "#bebe23",
+        textcolor: "white"
+    },
+
+    movie3: {
+        filesrc: "movie3",
+        title: "Sonic the Hedgehog",
+        length: "1h 40m",
+        rating: "93% audience score",
+        desc: "On a distant planet, Sonic, a young anthropomorphic blue hedgehog who can run at supersonic speed, is unexpectedly attacked by an echidna tribe. His guardian, an anthropomorphic owl named Longclaw, gives him a bag of warp rings that open portals to other planets. She uses one to send him to Earth while she stays behind to hold off the echidnas, leaving Sonic behind.<br>Ten years later, Sonic enjoys a secret life under the rural town of Green Hills, Montana, but longs to make friends. He idolizes the local sheriff Tom Wachowski and his wife Maddie, unaware the pair are to relocate to San Francisco as Tom plans to accept a post with the SFPD.<br>One night, Sonic grows upset over his loneliness while playing baseball alone and, while running at high speeds, accidentally triggers an electromagnetic pulse that causes a massive power outage across the Pacific Northwest. The U.S. Department of Defense reluctantly enlists the services of eccentric roboticist and scientific genius Dr. Robotnik to determine the cause. Seeing he is being hunted, Sonic reluctantly plans to leave Earth for a different planet only consisting of fungi.",
+        color: "#2359be",
+        textcolor: "white"
+    },
+
+    movie4: {
+        filesrc: "movie4",
+        title: "Tomb Raider",
+        length: "1h 58m",
+        rating: "55% audience score",
+        desc: "Following the disappearance of her father, Lord Richard Croft, Lara Croft makes a living as a bike courier. She is arrested after a collision with a police car. Richard's business partner Ana Miller posts her bail and urges Lara to accept her inheritance. Lara believes her father to still be alive, but Ana warns Lara that if she does not claim her inheritance, the family estate, Croft Manor, will be sold off. Lara reluctantly accepts and gains access to a secret chamber in her father's tomb, where she finds a pre-recorded video message from Richard detailing his research into Himiko, the mythical Queen of Yamatai who was said to command the power over life and death. Richard warns Lara to destroy his research, but she ignores his warnings and investigates further.<br>Not yet having received her inheritance Lara is forced to pawn the jade pendant her father gave her. With the money Lara travels to Hong Kong where she hires Lu Ren, captain of the ship Endurance, to sail into the Devil's Sea and the island of Yamatai. The ship capsizes in a violent storm and Lara has washed ashore where she is knocked unconscious. She awakens only to encounter Mathias Vogel, the leader of an expedition to locate Himiko's tomb. The expedition has been funded by a shadowy organization called Trinity, which seeks to harness and weaponize Himiko's power. Mathias takes Lara prisoner, claiming that he killed her father and revealing that he intends to use Richard's research to continue his expedition. He adds Lara and Lu to his slave force. The two of them try to escape, but only Lara succeeds.",
+        color: "#be2323",
+        textcolor: "white"
+    },
+
+    movie5: {
+        filesrc: "movie5",
+        title: "Pixels",
+        length: "1h 46m",
+        rating: "46% audience score",
+        desc: "At a video game arcade in 1982 with his friend Will Cooper, 13-year-old Sam Brenner seemingly loses a championship game of Donkey Kong to obnoxious Eddie 'The Fireblaster' Plant. Videocassette footage of the event is included in a time capsule launched into space. In the present day, Brenner is an electronics installer while Cooper is the unpopular President of the United States. In Guam, the Andersen Air Force Base is besieged by an extraterrestrial force attacking in the form of Galaga and a soldier is abducted.<br>Brenner works at the home of divorced U.S. Army lieutenant colonel Violet van Patten, and the two are separately summoned to the White House by Cooper. Upon seeing the video footage and meeting with Ludlow 'The Wonder Kid' Lamonsoff, Brenner deduces that the aliens have somehow mistaken the videocassette images for a declaration of war, and are attacking Earth with a strange technology that allows them to morph into the video games featured at the championship. The aliens interrupt a television broadcast using footage from the cassette, challenging the people of Earth to a battle where if the aliens win three times, they will conquer the planet. Brenner and Cooper are unable to stop one of the attacks as the Taj Mahal is damaged by the aliens in the form of Arkanoid and a bystander is abducted.",
+        color: "#237bbe",
+        textcolor: "white"
+    },
+
+    movie6: {
+        filesrc: "movie6",
+        title: "Tetris",
+        length: "1h 58m",
+        rating: "87% audience score",
+        desc: "In 1988, Henk Rogers of Bullet-Proof Software markets his newest video game at the Consumer Electronics Show in Las Vegas. While there, he instead becomes enamored with the game Tetris, created by Soviet programmer Alexey Pajitnov, who works for government-owned ELORG in the Soviet Union. Rogers explains to a bank manager that Robert Stein of Andromeda Software had obtained the worldwide licensing rights to Tetris from ELORG, and signed a contract with media tycoon Robert Maxwell and his son, CEO of Mirrorsoft, Kevin Maxwell, allowing them to distribute Tetris in exchange for game royalties. Meanwhile, the Mirrorsoft representative at CES had sold Rogers the Tetris rights in Japan for PC, console, and arcade.<br>Rogers obtains an audience with Nintendo CEO Hiroshi Yamauchi and proposes a partnership to produce Tetris for the Nintendo Famicom and arcade machines. Shortly after beginning production, Rogers receives a call from Kevin, explaining that the arcade rights had already been promised to Sega. Rogers returns to Yamauchi, asking for his residuals in advance. Instead, he is sent to Nintendo of America headquarters in Seattle, and shown Nintendo's upcoming handheld device, the Game Boy, which is planned to be released with Super Mario Land. Rogers convinces president Minoru Arakawa and senior VP Howard Lincoln to package it with Tetris instead, and promises to obtain the handheld licensing rights.",
+        color: "#fff",
+        textcolor: "black"
+    },
+
+}
+
+let moviesArray = Object.values(movies)
+const scrollerScroll = document.querySelector('.scroller-elements')
+
+let scrollerScrollPX = 700
+let currentScroll = -1
+let blockID = 0
+
+const timer = document.querySelector('.line')
+const timerLine = document.querySelector('.timer-line')
+
+let timeoutScroll
+let timeoutRemove
+
+const siteLogic = function() {
+    blockID++;
+    currentScroll++
+
+    if (!autoplay) {
+        return
+    }
+
+    // autoplayBlockAdded = !autoplayBlockAdded
+    if (currentScroll == 6) {
+        scrollerScroll.style.transition = `none`
+        scrollerScroll.style.transform = `translateX(-350px)`
+        currentScroll = 0
+        scrollerScrollPX = 700
+    }
+
+
+    setTimeout(() => {
+        scrollerScroll.style.transition = `all 0.3s`
+    }, 300);
+    
+    if (blockID == 6) blockID = 0
+    
+    console.log(currentScroll);
+    
+    let prevID = blockID
+    timer.style.backgroundColor = moviesArray[currentScroll].color
+    
+    let contentBlock = document.createElement(`div`)
+    contentBlock.classList.add('movie-container','inactive-container',`blockID${blockID}`)
+    contentBlock.innerHTML = `
+        <div class="movie-contentblock">
+        <div class="movie-background-splash">
+            <video class="movie-splash" autoplay muted loop>
+                <source src="src/splash/${moviesArray[currentScroll].filesrc}.mp4" type="video/mp4">
+            </video>
+        </div>
+        <div class="content">
+            <img class="movie-logo" src="src/logos/${moviesArray[currentScroll].filesrc}.png" alt="">
+            <div class="movie-info" style="color: white">
+                <p class="movie-title">${moviesArray[currentScroll].title}</p>
+                <p class="movie-stats">${moviesArray[currentScroll].length} | ${moviesArray[currentScroll].rating}</p>
+                <div class="movie-desc-container">
+                    <p class="movie-desc">${moviesArray[currentScroll].desc}</p>
+                </div>
+                <div class="movie-desc-btn-holder">
+                    <p class="movie-watch-btn" style="color: ${moviesArray[currentScroll].textcolor} ; background-color:${moviesArray[currentScroll].color}">Watch now!</p>
+                    <p class="movie-desc-btn">Learn more &downarrow;</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+    
+    setTimeout(() => {
+        contentBlock.classList.remove('inactive-container')
+        contentBlock.classList.add('active-container')
+        document.querySelector(`.scrollEl${currentScroll}`).classList.add('active-scrollEl')
+    }, 10);
+    document.querySelector('.scroller-container').appendChild(contentBlock)
+
+
+    
+    timeoutScroll = setTimeout(() => {
+        if (autoplay) {
+            scrollerScroll.style.transform = `translateX(-${(currentScroll * 350) + 700}px)`
+            scrollerScrollPX += 350
+            document.querySelector(`.scrollEl${currentScroll}`).classList.remove('active-scrollEl')
+        }
+    }, 9500);
+
+
+    timeoutRemove = setTimeout(() => {
+            let BlockToBeRemoved = document.querySelector(`.blockID${prevID}`);
+            // console.log(BlockToBeRemoved);
+
+             BlockToBeRemoved.parentNode.removeChild(BlockToBeRemoved);
+    }, 10300);
+}
+
+setTimeout(() => {
+siteLogic()
+}, 100);
+
+let autoplayInterval
+const autoplayIntervalStart = () => autoplayInterval = setInterval(siteLogic,10000)
+const autoplayIntervalStop = () => clearInterval(autoplayInterval)
+autoplayIntervalStart()
+
+const autoplayTogglerBtn = document.querySelector('.autoplay-btn-toggler')
+let autoplay = true
+const autoplayToggler = function() {
+    if (autoplayTogglerBtn.classList.contains('on') && autoplay) {
+        autoplayTogglerBtn.classList.remove('on')
+        autoplayTogglerBtn.classList.add('off')
+    } else if (autoplayTogglerBtn.classList.contains('off') && !autoplay) {
+        autoplayTogglerBtn.classList.add('on')
+        autoplayTogglerBtn.classList.remove('off')
+    }
+    autoplay = !autoplay
+    autoplayChecker()
+
+    // console.log(autoplay);
+}
+
+const autoplayChecker = function() {
+    if (autoplay) {
+        autoplayIntervalStart()
+        timerLine.style.display = 'block'
+    } else {
+        autoplayIntervalStop()
+        timerLine.style.display = 'none'
+        clearTimeout(timeoutScroll)
+        clearTimeout(timeoutRemove)
+    }
+}
+
+
+document.querySelector('.autoplay-btn').addEventListener('click', autoplayToggler)
+
+
+//
+    // if (currentScroll == 6) {
+    //     scrollerScroll.style.transition = `none`
+    //     scrollerScroll.style.transform = `translateX(-350px)`
+    //     currentScroll = 0
+    //     scrollerScrollPX = 700
+    // }
+
+
+    // setTimeout(() => {
+    //     scrollerScroll.style.transition = `all 0.3s`
+    // }, 300);
+
+    // if (blockID == 6) blockID = 0
+
+    // console.log(currentScroll);
+    
+    // let prevID = blockID
+        
+    // let contentBlock = document.createElement(`div`)
+    // contentBlock.classList.add('movie-container','inactive-container',`blockID${blockID}`)
+    // contentBlock.innerHTML = `
+    //     <div class="movie-contentblock">
+    //     <div class="movie-background-splash">
+    //         <video class="movie-splash" autoplay muted loop>
+    //             <source src="src/splash/${moviesArray[currentScroll].filesrc}.mp4" type="video/mp4">
+    //         </video>
+    //     </div>
+    //     <div class="content">
+    //         <img class="movie-logo" src="src/logos/${moviesArray[currentScroll].filesrc}.png" alt="">
+    //         <div class="movie-info" style="color: white">
+    //             <p class="movie-title">${moviesArray[currentScroll].title}</p>
+    //             <p class="movie-stats">${moviesArray[currentScroll].length} | ${moviesArray[currentScroll].rating}</p>
+    //             <div class="movie-desc-container">
+    //                 <p class="movie-desc">${moviesArray[currentScroll].desc}</p>
+    //             </div>
+    //             <div class="movie-desc-btn-holder">
+    //                 <p class="movie-watch-btn" style="color: ${moviesArray[currentScroll].textcolor} ; background-color:${moviesArray[currentScroll].color}">Watch now!</p>
+    //                 <p class="movie-desc-btn">Learn more &downarrow;</p>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
+    // `
+    // document.querySelector('.line').style.backgroundColor = moviesArray[currentScroll].color
+
+    // setTimeout(() => {
+    //     contentBlock.classList.remove('inactive-container')
+    //     contentBlock.classList.add('active-container')
+    //     document.querySelector(`.scrollEl${currentScroll}`).classList.add('active-scrollEl')
+    // }, 10);
+    // document.querySelector('.scroller-container').appendChild(contentBlock)
+    // blockID++;
+
+    // setTimeout(() => {
+    //     scrollerScroll.style.transform = `translateX(-${scrollerScrollPX}px)`
+    //     scrollerScrollPX += 350
+    //     document.querySelector(`.scrollEl${currentScroll}`).classList.remove('active-scrollEl')
+    //     currentScroll++
+    // }, 9500);
+
+    // setTimeout(() => {
+    //     let BlockToBeRemoved = document.querySelector(`.blockID${prevID}`)
+    //     console.log(BlockToBeRemoved);
+        
+    //     BlockToBeRemoved.parentNode.removeChild(BlockToBeRemoved)
+    // }, 10300);
